7779c74e2c68d9aa2366feba444df19cf47edce6
Palash Bairagi <palashbairagi11122@gmail.com>
2019-10-15 19:27:36 -0400

    Cors added

---
+++ src/main/java/com/cp/stackunderflow/config/CorsFilter.java
@@ -0,0 +1,21 @@
+package com.cp.stackunderflow.config;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+
+        chain.doFilter(request, response);
+    }
+}
