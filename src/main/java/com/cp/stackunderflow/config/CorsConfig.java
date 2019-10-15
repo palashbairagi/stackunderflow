da40bb880565cab3f5b0adecd47e50da23ff3361
Palash Bairagi <palashbairagi11122@gmail.com>
2019-10-15 15:49:58 -0400

    CORS config added

---
+++ src/main/java/com/cp/stackunderflow/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.cp.stackunderflow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
